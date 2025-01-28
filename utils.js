f35f4ba7f1cbe4a442723c1720e5f7d60b1b8adf
Vinay Maheshwari <vinaymaheshwari35@gmail.com>
2025-01-28 14:48:58 +0530

    ADD: Fixed base url

--- utils.js
+++ utils.js
@@ -5,7 +5,7 @@ export const articleStatus = {
 };
 
 
-// export const profileFile = "https://api.proximity.press/uploads/profile/"
-// export const articleFile = "https://api.proximity.press/uploads/article/"
-export const profileFile = "http://localhost:5000/uploads/profile/"
-export const articleFile = "http://localhost:5000/uploads/article/"
+export const profileFile = "https://api.proximity.press/uploads/profile/"
+export const articleFile = "https://api.proximity.press/uploads/article/"
+// export const profileFile = "http://localhost:5000/uploads/profile/"
+// export const articleFile = "http://localhost:5000/uploads/article/"
