b05942899579e2ec886b6d6125928cd36b02d9e6
Vinay Maheshwari <vinaymaheshwari35@gmail.com>
2025-02-07 15:06:04 +0530

    ADD: Fixed schema

--- models/userModel.js
+++ models/userModel.js
@@ -26,7 +26,7 @@ const userSchema = new mongoose.Schema(
             default:
               "What fields or industries do you work or will you work in?",
           },
-          mandatory: true,
+          mandatory: { type: Boolean, default: false },
           answer: { type: String, default: "" },
         },
         2: {
@@ -34,7 +34,7 @@ const userSchema = new mongoose.Schema(
             type: String,
             default: "Could you please describe your role or job title?",
           },
-          mandatory: true,
+          mandatory: { type: Boolean, default: false },
           answer: { type: String, default: "" },
         },
         3: {
@@ -43,7 +43,7 @@ const userSchema = new mongoose.Schema(
             default:
               "What specific areas of your craft or industry do you have the most knowledge in? List them in high to low order.",
           },
-          mandatory: true,
+          mandatory: { type: Boolean, default: false },
           answer: { type: String, default: "" },
         },
       },
@@ -54,7 +54,7 @@ const userSchema = new mongoose.Schema(
             default:
               "What specific areas of your craft or industry do you have the most knowledge in? List them in high to low order.",
           },
-          mandatory: true,
+          mandatory: { type: Boolean, default: false },
           answer: { type: String, default: "" },
         },
         2: {
@@ -63,7 +63,7 @@ const userSchema = new mongoose.Schema(
             default:
               "What specialized skills or experience do you have or can develop that most other people don’t have, or do not think about having? Please describe in detail.",
           },
-          mandatory: true,
+          mandatory: { type: Boolean, default: false },
           answer: { type: String, default: "" },
         },
         3: {
@@ -72,7 +72,7 @@ const userSchema = new mongoose.Schema(
             default:
               "What other variations of applications can your skills be applied to?",
           },
-          mandatory: true,
+          mandatory: { type: Boolean, default: false },
           answer: { type: String, default: "" },
         },
         4: {
@@ -81,7 +81,7 @@ const userSchema = new mongoose.Schema(
             default:
               "What are all the possible end applications of your past and present work?",
           },
-          mandatory: true,
+          mandatory: { type: Boolean, default: false },
           answer: { type: String, default: "" },
         },
         5: {
@@ -90,7 +90,7 @@ const userSchema = new mongoose.Schema(
             default:
               "Please enlist all of your published work within the context of the subject matter being covered in this article (Research Papers, Scholarly articles, Media coverage, Blogs, etc.).",
           },
-          mandatory: true,
+          mandatory: { type: Boolean, default: true },
           answer: { type: String, default: "" },
         },
       },
@@ -101,7 +101,7 @@ const userSchema = new mongoose.Schema(
             default:
               "What about the problems that you can apply your skills to that are not working well right now? How could it improve?",
           },
-          mandatory: true,
+          mandatory: { type: Boolean, default: false },
           answer: { type: String, default: "" },
         },
         2: {
@@ -110,7 +110,7 @@ const userSchema = new mongoose.Schema(
             default:
               "What are the gaps in your industry or workplace in general? What areas are underserved? Please explain.",
           },
-          mandatory: true,
+          mandatory: { type: Boolean, default: false },
           answer: { type: String, default: "" },
         },
         3: {
@@ -119,7 +119,7 @@ const userSchema = new mongoose.Schema(
             default:
               "What are some major challenges that you successfully overcame as part of your involvement in the concerned area of work, which in turn allowed you to achieve a great result? ",
           },
-          mandatory: true,
+          mandatory: { type: Boolean, default: false },
           answer: { type: String, default: "" },
         },
       },
@@ -130,7 +130,7 @@ const userSchema = new mongoose.Schema(
             default:
               "What professional achievements have you secured while working on the subject matter of this article?   *(List major achievements: scaling the corporate/academic ladder, associating with major organizations, etc.)*",
           },
-          mandatory: true,
+          mandatory: { type: Boolean, default: false },
           answer: { type: String, default: "" },
         },
         2: {
@@ -139,7 +139,7 @@ const userSchema = new mongoose.Schema(
             default:
               "What are five of the biggest impactful projects you have worked on, and what are your proudest career accomplishments? ",
           },
-          mandatory: true,
+          mandatory: { type: Boolean, default: false },
           answer: { type: String, default: "" },
         },
         3: {
@@ -148,7 +148,7 @@ const userSchema = new mongoose.Schema(
             default:
               "How have you been able to create an impact at your workplace through your work in the capacity of a crucial member of your organization, by being involved and participating in the area of work that this particular article would cover?  *(Enlist and describe impact in terms of tangible metrics like cost savings, revenue increments, efficiency increments, etc.)*",
           },
-          mandatory: true,
+          mandatory: { type: Boolean, default: false },
           answer: { type: String, default: "" },
         },
         4: {
@@ -157,7 +157,7 @@ const userSchema = new mongoose.Schema(
             default:
               "What are some of your works or results that can be measured in quantifiable terms (in the context of the subject matter at hand)? Also share the quantified figures.",
           },
-          mandatory: true,
+          mandatory: { type: Boolean, default: false },
           answer: { type: String, default: "" },
         },
         5: {
@@ -166,7 +166,7 @@ const userSchema = new mongoose.Schema(
             default:
               "How can you demonstrate that you are THE expert? Describe the specific solutions and results you can provide.",
           },
-          mandatory: true,
+          mandatory: { type: Boolean, default: false },
           answer: { type: String, default: "" },
         },
         6: {
@@ -175,7 +175,7 @@ const userSchema = new mongoose.Schema(
             default:
               "What specific solutions are other people working on or providing in your field that are working well?",
           },
-          mandatory: true,
+          mandatory: { type: Boolean, default: false },
           answer: { type: String, default: "" },
         },
       },
@@ -186,7 +186,7 @@ const userSchema = new mongoose.Schema(
             default:
               "From the standpoint of an experienced professional in this particular arena/category of work, what are your original thoughts and insights in relevance to what you do/have done?  *(You may also share insights on current or upcoming trends and practices and firsthand suggestions from having worked on major projects.)*",
           },
-          mandatory: true,
+          mandatory: { type: Boolean, default: false },
           answer: { type: String, default: "" },
         },
         2: {
@@ -195,7 +195,7 @@ const userSchema = new mongoose.Schema(
             default:
               "What aspects are most important to you when it comes to your work and the products/services that you work on?",
           },
-          mandatory: true,
+          mandatory: { type: Boolean, default: false },
           answer: { type: String, default: "" },
         },
         3: {
@@ -204,7 +204,7 @@ const userSchema = new mongoose.Schema(
             default:
               "List out the approximate size of each of the spaces/fields. How many people are working? If you are to compete with others in specific niches, how many other people would you be competing against in each case?",
           },
-          mandatory: true,
+          mandatory: { type: Boolean, default: false },
           answer: { type: String, default: "" },
         },
       },
