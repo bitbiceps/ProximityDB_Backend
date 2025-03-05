8f3e379cc586417352626e43baac4b5f3cc3ecd4
sahilbisht-saimanshetty <sahil.b@saimanshetty.com>
2025-03-05 14:14:31 +0530

    image bug sovle , user update profile , job title options added

--- helpers/utils.js
+++ helpers/utils.js
@@ -4,10 +4,10 @@ export const articleStatus = {
   completed: "completed",
 };
 
-export const profileFile = "https://api.proximity.press/uploads/profile/";
-export const articleFile = "https://api.proximity.press/uploads/article/";
-// export const profileFile = "http://localhost:5000/uploads/profile/"
-// export const articleFile = "http://localhost:5000/uploads/article/"
+// export const profileFile = "https://api.proximity.press/uploads/profile/";
+// export const articleFile = "https://api.proximity.press/uploads/article/";
+export const profileFile = "http://localhost:5000/uploads/profile/"
+export const articleFile = "http://localhost:5000/uploads/article/"
 
 export const socketEvents = {
   TEST__BROADCAST: "TEST__BROADCAST",
