4f3960d8b89ca369e38905268c2fae9abbf33b10
sahilbisht-saimanshetty <sahil.b@saimanshetty.com>
2025-06-16 11:31:36 +0530

    internal added in the cors

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 // Middleware
 app.use(cors({origin: process.env.NODE_ENV === "production" 
-      ? [process.env.FRONTEND_URL, "https://staging.dashboard.proximity.press" , "https://staging.proximity.press" ,"https://proximity.press"] 
+      ? [process.env.FRONTEND_URL, "https://staging.dashboard.proximity.press" , "https://staging.proximity.press" , "https://proximity.press", "https://internal.proximity.press"] 
       : true ,
      credentials: true }));
 
