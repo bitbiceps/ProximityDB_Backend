05cbf680c0a8b20c4003b71a7377d369a9ea36a4
sahilbisht-saimanshetty <sahil.b@saimanshetty.com>
2025-07-04 13:13:39 +0530

    update in the  profileImage modal

--- models/profileImageModel.js
+++ models/profileImageModel.js
@@ -4,6 +4,7 @@ const profileImageSchema = new mongoose.Schema({
   filename: { type: String, required: true },
   filepath: { type: String, required: true },
   user: { type: mongoose.Types.ObjectId, ref: "User" },
+  team : {type : mongoose.Types.ObjectId, ref : "Team"}
 });
 
 export default mongoose.model("ProfileImage", profileImageSchema);
